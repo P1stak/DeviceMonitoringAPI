a53fb0807f2b1fc09454967f49c830521c33e28c
P1stak <shik213mainl.ru>
2025-08-08 13:15:13 +0700

    добавлен класс репозиторий с ассинхронщиной

--- DeviceMonitoring/Models/Device.cs
+++ DeviceMonitoring/Models/Device.cs
@@ -2,10 +2,10 @@
 {
     public class Device
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = new Guid();
         public string Name { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public string Version { get; set; }
+        public string Version { get; set; } = string.Empty;
     }
 }
