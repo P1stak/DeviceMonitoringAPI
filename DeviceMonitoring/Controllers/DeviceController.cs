b9599c3e72416b1deb653a0ef3ec546f3e36b69e
P1stak <shik213mainl.ru>
2025-08-08 12:45:48 +0700

    Add project files.

---
+++ DeviceMonitoring/Controllers/DeviceController.cs
@@ -0,0 +1,41 @@
+﻿using DeviceMonitoring.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceMonitoring.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeviceController : Controller
+    {
+        private List<Device> _devices = new List<Device>();
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                _devices.Add(device);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_devices);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTryId(Guid id)
+        {
+            var device = _devices.Find(dev => dev.Id == id);
+
+            if (device == null)
+            { 
+                return NotFound();
+            }
+            return Ok(device);
+        }
+    }
+}
